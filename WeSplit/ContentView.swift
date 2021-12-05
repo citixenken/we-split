010ffd88c8604d91fb80e468f6a4b2141c74a4e2
citixenken <citizenken@Kens-MacBook-Air.local>
2021-12-05 12:43:06 +0300

    Adding segmented controls

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,62 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     
-//    Example Code snippets
+    //    Example Code snippets
     
-//    @State private var tapCount = 0
-//    @State private var name = ""
-//
-//    let progLanguages = ["Bash", "Java", "Kotlin", "Swift", "Java", "R"]
-//    @State private var selectedLanguage = "Swift"
-//
-//    var body: some View {
-//        NavigationView {
-//            Form {
-//                Section {
-//                    Text("We Split").padding()
-//                    Text("We Split").padding()
-//                    Text("We Split").padding()
-//                    Text("We Split").padding()
-//                    Text("We Split").padding()
-//
-//                }
-//                Section {
-//                    Text("We Splot").padding()
-//                    Text("We Splot").padding()
-//                    Text("We Splot").padding()
-//
-//                }
-//                Section {
-//                    Button ("Tap Count: \(tapCount)") {
-//                        self.tapCount += 2
-//                    }
-//                }
-//                Section {
-//                    TextField("Enter your name: ", text: $name)
-//                    Text("Hello \(name); Hope you're enjoying iOS development")
-//                }
-//
-//                Section {
-////                    ForEach(1..<6) { number in
-////                        Text("We're at count: \(number)")
-////                    }
-//                    ForEach(1..<6) {
-//                        Text("We're at count number: \($0)")
-//                    }
-//                }
-//
-//                Section {
-//                    Picker("Select your favourite language: ", selection: $selectedLanguage) {
-//                        ForEach(progLanguages, id: \.self) {
-//                            Text($0)
-//                        }
-//                    }
-//                }
-//            }
-//            .navigationTitle("We Split")
-//            .navigationBarTitleDisplayMode(.inline)
-//        }
-//    }
+    //    @State private var tapCount = 0
+    //    @State private var name = ""
+    //
+    //    let progLanguages = ["Bash", "Java", "Kotlin", "Swift", "Java", "R"]
+    //    @State private var selectedLanguage = "Swift"
+    //
+    //    var body: some View {
+    //        NavigationView {
+    //            Form {
+    //                Section {
+    //                    Text("We Split").padding()
+    //                    Text("We Split").padding()
+    //                    Text("We Split").padding()
+    //                    Text("We Split").padding()
+    //                    Text("We Split").padding()
+    //
+    //                }
+    //                Section {
+    //                    Text("We Splot").padding()
+    //                    Text("We Splot").padding()
+    //                    Text("We Splot").padding()
+    //
+    //                }
+    //                Section {
+    //                    Button ("Tap Count: \(tapCount)") {
+    //                        self.tapCount += 2
+    //                    }
+    //                }
+    //                Section {
+    //                    TextField("Enter your name: ", text: $name)
+    //                    Text("Hello \(name); Hope you're enjoying iOS development")
+    //                }
+    //
+    //                Section {
+    ////                    ForEach(1..<6) { number in
+    ////                        Text("We're at count: \(number)")
+    ////                    }
+    //                    ForEach(1..<6) {
+    //                        Text("We're at count number: \($0)")
+    //                    }
+    //                }
+    //
+    //                Section {
+    //                    Picker("Select your favourite language: ", selection: $selectedLanguage) {
+    //                        ForEach(progLanguages, id: \.self) {
+    //                            Text($0)
+    //                        }
+    //                    }
+    //                }
+    //            }
+    //            .navigationTitle("We Split")
+    //            .navigationBarTitleDisplayMode(.inline)
+    //        }
+    //    }
     
     @State private var totalAmount = 0.0
     @State private var numberOfPeople = 2
@@ -79,22 +79,28 @@ struct ContentView: View {
                     //TextField("Enter total cost", value: $totalAmount, format: .currency(code: "KSH"))
                     TextField("Total cost", value: $totalAmount, format:
                                     .currency(code: Locale.current.currencyCode ?? "USD"))
-                                    .keyboardType(.decimalPad)
+                        .keyboardType(.decimalPad)
                     Picker("Number of people", selection: $numberOfPeople) {
                         ForEach(2..<100) {
                             Text("\($0) people")
                         }
                     }
-    //                Picker("Tip percentage", selection: $tipPercentage) {
-    //                    ForEach (tipPercentages, id: \.self) {
-    //                        Text($0)
-    //                    }
-    //                }
                 }
-                Section {
-                    Text(totalAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                Section{
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach (tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip do you want to leave?")
                 }
             }
+            Section {
+                Text(totalAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+            }
+            
             .navigationTitle("WeSplit")
         }
         
