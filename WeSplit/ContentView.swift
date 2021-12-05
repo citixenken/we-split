ad9fb1cbbbee7a0c32616ade9594939e496b34a5
citixenken <citizenken@Kens-MacBook-Air.local>
2021-12-05 09:58:21 +0300

    Creating views in a loop

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -12,6 +12,9 @@ struct ContentView: View {
     @State private var tapCount = 0
     @State private var name = ""
     
+    let progLanguages = ["Bash", "Java", "Kotlin", "Swift", "Java", "R"]
+    @State private var selectedLanguage = "Swift"
+    
     var body: some View {
         NavigationView {
             Form {
@@ -29,13 +32,32 @@ struct ContentView: View {
                     Text("We Splot").padding()
                     
                 }
-                
-                Button ("Tap Count: \(tapCount)") {
-                    self.tapCount += 2
+                Section {
+                    Button ("Tap Count: \(tapCount)") {
+                        self.tapCount += 2
+                    }
+                }
+                Section {
+                    TextField("Enter your name: ", text: $name)
+                    Text("Hello \(name); Hope you're enjoying iOS development")
                 }
                 
-                TextField("Enter your name: ", text: $name)
-                Text("Hello \(name); Hope you're enjoying iOS development")
+                Section {
+//                    ForEach(1..<6) { number in
+//                        Text("We're at count: \(number)")
+//                    }
+                    ForEach(1..<6) {
+                        Text("We're at count number: \($0)")
+                    }
+                }
+                
+                Section {
+                    Picker("Select your favourite language: ", selection: $selectedLanguage) {
+                        ForEach(progLanguages, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
             }
             .navigationTitle("We Split")
             .navigationBarTitleDisplayMode(.inline)
