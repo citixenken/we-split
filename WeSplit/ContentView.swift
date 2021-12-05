c109e96617695073bedee4e86d741b79e464b20d
citixenken <citizenken@Kens-MacBook-Air.local>
2021-12-05 08:28:45 +0300

    first commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Ken Muyesu on 30/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Form {
+            Section {
+                Text("We Split").padding()
+                Text("We Split").padding()
+                Text("We Split").padding()
+                Text("We Split").padding()
+                Text("We Split").padding()
+               
+            }
+            Section {
+                Text("We Splot").padding()
+                Text("We Splot").padding()
+                Text("We Splot").padding()
+                
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
